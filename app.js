6552eceab174f1af45b0f625aef90556992304de
khatibmahmud49@gmail.com <khatibmahmud49@gmail.com>
2022-06-08 23:44:49 +0600

    second

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
-const getNote = require("./note")
+const chalk = require('chalk');
+const getNote = require("./note");
+const msg = getNote()
 
-console.log(getNote());
+console.log("START CODING");
+const command = process.argv[1];
+
+console.log(command);
